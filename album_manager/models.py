a389abf2f30452883b5fd47e4567776cc21de639
Alee234324444444444444444444 <apbarrionuevo@puce.edu.ec>
2024-08-05 21:44:16 -0500

    ESta con imagen

--- album_manager/models.py
+++ album_manager/models.py
@@ -4,7 +4,7 @@ class Artist(models.Model):
     name = models.CharField(max_length=40, null=False)
     country = models.CharField(max_length=30, null=False)
     birth = models.DateField(null=False)
-    picture = models.ImageField(upload_to='artist_images')
+    picture = models.ImageField(upload_to='artist_images/')
     
     def __str__(self) -> str:
         return self.name
@@ -14,7 +14,7 @@ class Album(models.Model):
     year = models.DateField(null=False)
     gender = models.CharField(max_length=30, null=False)
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
-    picture = models.ImageField(upload_to='album_images')
+    picture = models.ImageField(upload_to='album_images/')
     
     def __str__(self) -> str:
         return self.title
--- album_manager/views.py
+++ album_manager/views.py
@@ -1,26 +1,32 @@
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, redirect, render
-
+from django.template import loader
+from django.shortcuts import get_object_or_404, redirect
 from .forms import AlbumForm, ArtistForm
 from .models import Artist, Album
 
 def index(request):
-    albums = Album.objects.order_by('title') 
-    return render(request, 'index.html', {'albums': albums})
+    albums = Album.objects.order_by('title')
+    template = loader.get_template('index.html')
+    context = {'albums': albums}
+    return HttpResponse(template.render(context, request))
 
 def album(request, album_id):
-    album = get_object_or_404(Album, pk=album_id)  
-    return render(request, 'display_album.html', {'album': album})
+    album = get_object_or_404(Album, pk=album_id)
+    template = loader.get_template('display_album.html')
+    context = {'album': album}
+    return HttpResponse(template.render(context, request))
 
 def add_album(request):
     if request.method == 'POST':
         form = AlbumForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('album_manager:index') 
+            return redirect('album_manager:index')
     else:
         form = AlbumForm()
-    return render(request, 'album_form.html', {'form': form})
+    template = loader.get_template('album_form.html')
+    context = {'form': form}
+    return HttpResponse(template.render(context, request))
 
 def edit_album(request, id):
     album = get_object_or_404(Album, pk=id)
@@ -28,33 +34,41 @@ def edit_album(request, id):
         form = AlbumForm(request.POST, request.FILES, instance=album)
         if form.is_valid():
             form.save()
-            return redirect('album_manager:index')  
+            return redirect('album_manager:index')
     else:
         form = AlbumForm(instance=album)
-    return render(request, 'album_form.html', {'form': form})
+    template = loader.get_template('album_form.html')
+    context = {'form': form}
+    return HttpResponse(template.render(context, request))
 
 def delete_album(request, id):
     album = get_object_or_404(Album, pk=id)
     album.delete()
-    return redirect('album_manager:index') 
+    return redirect('album_manager:index')
 
 def artists_list(request):
     artists = Artist.objects.all()
-    return render(request, 'artists_list.html', {'artists': artists})
+    template = loader.get_template('artists_list.html')
+    context = {'artists': artists}
+    return HttpResponse(template.render(context, request))
 
 def artist(request, artist_id):
     artist = get_object_or_404(Artist, pk=artist_id)
-    return render(request, 'display_artist.html', {'artist': artist})
+    template = loader.get_template('display_artist.html')
+    context = {'artist': artist}
+    return HttpResponse(template.render(context, request))
 
 def add_artist(request):
     if request.method == 'POST':
         form = ArtistForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('album_manager:artists_list')  
+            return redirect('album_manager:artists_list')
     else:
         form = ArtistForm()
-    return render(request, 'artist_form.html', {'form': form})
+    template = loader.get_template('artist_form.html')
+    context = {'form': form}
+    return HttpResponse(template.render(context, request))
 
 def edit_artist(request, id):
     artist = get_object_or_404(Artist, pk=id)
@@ -62,12 +76,15 @@ def edit_artist(request, id):
         form = ArtistForm(request.POST, request.FILES, instance=artist)
         if form.is_valid():
             form.save()
-            return redirect('album_manager:artists_list')  
+            return redirect('album_manager:artists_list')
     else:
         form = ArtistForm(instance=artist)
-    return render(request, 'artist_form.html', {'form': form})
+    template = loader.get_template('artist_form.html')
+    context = {'form': form}
+    return HttpResponse(template.render(context, request))
 
 def delete_artist(request, id):
     artist = get_object_or_404(Artist, pk=id)
     artist.delete()
-    return redirect('album_manager:artists_list')  
+    return redirect('album_manager:artists_list')
+
